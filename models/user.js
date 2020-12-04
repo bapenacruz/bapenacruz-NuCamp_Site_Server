add0c9edf6a222b54f9f086891b9bd7bc76b93d8
Bryan <bapenacruz@gmail.com>
2020-12-04 14:33:34 -0500

    Mongoose Population

--- models/user.js
+++ models/user.js
@@ -6,6 +6,18 @@ const userSchema = new Schema({
     admin: {
         type: Boolean,
         default: false
+    },
+    firstname: {
+        type: String,
+        default: ''
+    },
+    lastname: {
+        type: String,
+        default: ''
+    },
+    admin: {
+        type: Boolean,
+        default: false
     }
 });
 
