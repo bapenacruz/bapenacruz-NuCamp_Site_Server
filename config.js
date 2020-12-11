46ba021530822d6f123cd4f8aac9fbcbea33abbb
Bryan <bapenacruz@gmail.com>
2020-12-11 18:33:53 -0500

    FB OAuth and User Authentication

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '193210472458947',
+        clientSecret: 'bd0025e6d6d8267a6919053785644cf0'
+    }
 }
